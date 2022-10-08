67eadff5f58b7d19e0a03acb61143f075510ec17
parikshitsm <mancharesp@gmail.com>
2022-10-08 14:05:47 +0530

    Delete was not working as expected. Code fix added for the same.

--- src/main/java/dev/michaellamb/demo/service/ImageService.java
+++ src/main/java/dev/michaellamb/demo/service/ImageService.java
@@ -5,12 +5,12 @@ import java.io.IOException;
 public interface ImageService {
     public String saveAsJpeg(String svgUri) throws Exception;
 
-    public byte[] getJpegFile(String fileName) throws IOException;
-
-    public void deleteJpegFile(String fileName) throws Exception;
-
     public String saveAsPng(String svgUri) throws Exception;
 
     public String saveAsTiff(String svgUri) throws Exception;
+
+    public byte[] getImageFile(String fileName) throws IOException;
+
+    public void deleteImageFile(String fileName) throws Exception;
 
 }
