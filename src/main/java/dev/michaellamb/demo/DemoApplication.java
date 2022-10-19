eb80128a1e2de2a610fb9a8edb725d11de102fea
Michael Lamb <michaellambgelo@users.noreply.github.com>
2022-10-19 09:21:57 -0500

    remove unused imports

--- src/main/java/dev/michaellamb/demo/DemoApplication.java
+++ src/main/java/dev/michaellamb/demo/DemoApplication.java
@@ -8,8 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
